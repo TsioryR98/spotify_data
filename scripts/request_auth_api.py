763d0a24565d64cd1b0deb205b0ae3ead417df71
TsioryRamiadamahefa <153943203+TsioryR98@users.noreply.github.com>
2025-09-09 15:38:00 +0000

    feat: add utils for requesting auth from spotify api

---
+++ scripts/request_auth_api.py
@@ -0,0 +1,28 @@
+import base64
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+
+# token as need base 64 encoded string
+auth_str = f"{client_id}:{client_secret}".encode("utf-8")
+auth_base64 = base64.b64encode(auth_str).decode("utf-8")
+
+url = "https://accounts.spotify.com/api/token"
+
+headers = {
+    "Authorization": f"Basic  {auth_base64}",
+    "Content-Type": "application/x-www-form-urlencoded",
+}
+data = {"grant_type": "client_credentials"}
+
+response = requests.post(url, headers=headers, data=data)
+
+token = response.json()["access_token"]
+
+print(f"token : {token}")
