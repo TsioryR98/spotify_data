5205969313a5c1f3f46595c27ecc7dc1b035c3f0
TsioryRamiadamahefa <153943203+TsioryR98@users.noreply.github.com>
2025-09-22 17:11:06 +0000

    fix: fix function and return for token

--- scripts/request_auth_api.py
+++ scripts/request_auth_api.py
@@ -9,20 +9,23 @@ load_dotenv()
 client_id = os.getenv("client_id")
 client_secret = os.getenv("client_secret")
 
-# token as need base 64 encoded string
-auth_str = f"{client_id}:{client_secret}".encode("utf-8")
-auth_base64 = base64.b64encode(auth_str).decode("utf-8")
 
-url = "https://accounts.spotify.com/api/token"
+def request_auth_api(client_id: str, client_secret: str) -> str:
 
-headers = {
-    "Authorization": f"Basic  {auth_base64}",
-    "Content-Type": "application/x-www-form-urlencoded",
-}
-data = {"grant_type": "client_credentials"}
+    # token as need base 64 encoded string
+    auth_str = f"{client_id}:{client_secret}".encode("utf-8")
+    auth_base64 = base64.b64encode(auth_str).decode("utf-8")
 
-response = requests.post(url, headers=headers, data=data)
+    url = "https://accounts.spotify.com/api/token"
 
-token = response.json()["access_token"]
+    headers = {
+        "Authorization": f"Basic  {auth_base64}",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    data = {"grant_type": "client_credentials"}
 
-print(f"token : {token}")
+    response = requests.post(url, headers=headers, data=data)
+
+    token = response.json()["access_token"]
+
+    return token
