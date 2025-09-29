698aa5e88d099eac1eee5d439ce82f9c3c7e18d8
TsioryR98 <hei.tsiory.2@gmail.com>
2025-09-29 23:28:32 +0300

    fix : fix arguments for extract mod and output dir

---
+++ dags/spotify_etl_dag.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from datetime import datetime
+import os
+
+from utils.extract_spotify_data import extract_spotify_data
+from utils.request_auth_api import request_auth_api
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("client_id")
+CLIENT_SECRET = os.getenv("client_secret")
+BASE_OUTPUT_DIR = os.getenv("base_output_dir")
+
+
+defautls_args = {
+    "start_date": datetime(2024, 1, 1),
+    "owner": "airflow",
+    "depends_on_past": False,
+    "retries": 0,
+}
+
+with DAG(
+    dag_id="spotify_etl_dag",
+    description="ETL 6:00AM daily spotify data",
+    schedule="0 6 * * *",
+    default_args=defautls_args,
+    catchup=False,
+    tags=["spotify", "etl"],
+) as dag:
+    token = request_auth_api(CLIENT_ID, CLIENT_SECRET)
+
+    extract = extract_spotify_data(
+        token=token,
+        time_range="2015-2025",
+        types=["track", "album"],
+        markets=["US", "FR", "DE", "CA", "BR", "JP", "GB"],
+        output_dir=BASE_OUTPUT_DIR,
+    )
+    token >> extract
