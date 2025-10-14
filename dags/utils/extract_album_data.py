1d17f99f65a170d8925d74ae70b1fa6f3ad17524
TsioryR98 <hei.tsiory.2@gmail.com>
2025-10-15 00:23:12 +0300

    fix : fix pararms and data labels

--- dags/utils/extract_album_data.py
+++ dags/utils/extract_album_data.py
@@ -32,62 +32,53 @@ def extract_album_data(
 
             data = response.json()
 
-            items_key = f"{types}s"
+            album_datas = []  # store data in a list
 
-            if items_key in data and "items" in data[items_key]:
-                items = data[items_key]["items"]
-                if items:
-                    album_datas = []  # store data in a list
-                    for artist in items["artists"]:
+            items = data[f"{types}s"]["items"]
+            if items:
+                for album in items:
+                    for artist in album["artists"]:
                         album_data = {
-                            "album_id": items["id"],
-                            "album_type": items["album_type"],
-                            "total_tracks": items["total_tracks"],
-                            "name": items["name"],
-                            "release_date": items["release_date"],
-                            "market": items["market"],
+                            "album_id": album["id"],
+                            "album_type": album["album_type"],
+                            "total_tracks": album["total_tracks"],
+                            "name": album["name"],
+                            "release_date": album["release_date"],
+                            "market": market,
                             "artist_id": artist["id"],
                             "artist_name": artist["name"],
-                            "extraction_time": datetime.fromtimestamp(
-                                items["extraction_time"]
-                            ).strftime("%Y-%m-%d %H:%M:%S"),
+                            "extraction_time": datetime.now().strftime(
+                                "%Y-%m-%d %H:%M:%S"
+                            ),
                         }
                         album_datas.append(album_data)
 
-                    df = pd.DataFrame(album_datas, index=[0])
+                df = pd.DataFrame(album_datas)
 
-                    # create directory for each types and markets
-                    current_date = datetime.now().strftime("%Y-%m-%d")
+                # create directory for each types and markets
+                current_date = datetime.now().strftime("%Y-%m-%d")
 
-                    subdir = os.path.join(
-                        output_dir, "data", "raw", market, current_date
-                    )
-                    os.makedirs(subdir, exist_ok=True)
+                subdir = os.path.join(output_dir, "data", "raw", market, current_date)
+                os.makedirs(subdir, exist_ok=True)
 
-                    filename = os.path.join(subdir, f"search_{types}.csv")
+                filename = os.path.join(subdir, f"search_{types}.csv")
 
-                    # CSV output
-                    df.to_csv(filename, index=False)
+                # CSV output
+                df.to_csv(filename, index=False)
 
-                    # JSON output
-                    json_path = os.path.join(subdir, f"search_{types}.json")
-                    df.to_json(
-                        json_path,
-                        orient="records",
-                        lines=False,
-                        indent=2,
-                        force_ascii=False,
-                    )
+                # JSON output
+                json_path = os.path.join(subdir, f"search_{types}.json")
+                df.to_json(
+                    json_path,
+                    orient="records",
+                    lines=False,
+                    indent=2,
+                    force_ascii=False,
+                )
 
-                    logging.info(f"{types.upper()} data for {market}")
-
-                else:
-                    logging.warning(f"{types} data for {market} not found")
-            else:
-                logging.warning(f" Invalid response for {types} in {market}")
+                logging.info(f"{types.upper()} data for {market}")
         return True
-
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         logging.error(f"error for extracting : {str(e)}")
 
         return False
