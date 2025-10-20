86b0ae58070a67bb82dd128584a3fd011e5f401d
TsioryR98 <hei.tsiory.2@gmail.com>
2025-10-20 23:00:06 +0300

    feat: def for extracting album details and save into csv file as details

--- dags/utils/extract_album_details.py
+++ dags/utils/extract_album_details.py
@@ -2,6 +2,8 @@ import logging
 from typing import List
 import os
 import pandas as pd
+from datetime import datetime
+
 
 import requests
 from airflow.decorators import task
@@ -37,6 +39,56 @@ def extract_album_details(
                 response.raise_for_status()
 
                 data = response.json()
+                album_details = []
+                album_tracks = []
+                album_artists = []
+
+                # album detail format
+                album_detail = {
+                    "id": data["id"],
+                    "album_type": data["album_type"],
+                    "total_tracks": data["total_tracks"],
+                    "name": data["name"],
+                    "release_date": data["release_date"],
+                    "popularity": data["popularity"],
+                }
+                album_details.append(album_detail)
+                # album tracks format
+                album_id = data["id"]
+                total_track = data["tracks"]["total"]
+                for track in data["tracks"]["items"]:
+                    album_track = {
+                        "id": album_id,
+                        "track_id": track["id"],
+                        "duration_ms": track["duration_ms"],
+                        "name": track["name"],
+                        "disc_number": track["disc_number"],
+                        "total": total_track,
+                    }
+                    album_tracks.append(album_track)
+                # album tracks format
+                for artist in data["artists"]:
+                    album_artist = {
+                        "id": album_id,
+                        "artist_id": artist["id"],
+                        "artist_name": artist["name"],
+                    }
+                    album_artists.append(album_artist)
+
+        df_album_details = pd.DataFrame(album_details)
+        df_album_tracks = pd.DataFrame(album_tracks)
+        df_album_artists = pd.DataFrame(album_artists)
+
+        current_date = datetime.now().strftime("%Y-%m-%d")
+
+        subdir = os.path.join(output_dir, "data", "raw", market, current_date)
+        os.makedirs(subdir, exist_ok=True)
+
+        df_album_details.to_csv(subdir, "albums_details.csv", index=False)
+        df_album_tracks.to_csv(subdir, "df_album_tracks.csv", index=False)
+        df_album_artists.to_csv(subdir, "df_album_artists.csv", index=False)
+
+        logging.info(f"data for {market} done")
 
     except requests.exceptions.RequestException as e:
         logging.error(f"error for extracting : {str(e)}")
